1dadeb06ed6fc0c6d460049aa93f06dadd1d904b
Felipe Colacioppo <felipe.colacioppo@outlook.com>
2022-11-22 10:34:20 -0300

    Adicionar arquivos de projeto.

---
+++ CursoUdemy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace CursoUdemy
+{
+
+    internal class Program
+    {
+
+        static void Main(String[] args)
+        {
+
+            Triangulo t1 = new Triangulo();
+
+            double areaTriangulo;
+
+            Console.WriteLine("valor do lado A");
+            t1.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("valor do lado B");
+            t1.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("valor do lado C");
+            t1.C = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            areaTriangulo = t1.CalcularArea();
+
+            Console.WriteLine();
+            Console.WriteLine("Área do triângulo = " + areaTriangulo.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+
+}
---
+++ CursoUdemy/Triangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CursoUdemy
+{
+
+    internal class Triangulo
+    {
+
+        public double A;
+        public double B;
+        public double C;
+
+
+        public double CalcularArea()
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return raiz;
+        }
+
+    }
+
+}
