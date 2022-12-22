814b858da20223ed1150afccd1ec0a4f6aa5ca5f
Felipe Colacioppo <felipe.colacioppo@outlook.com>
2022-12-22 16:21:49 -0300

    Update Herança

---
+++ CursoUdemy/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿namespace CursoUdemy.Entities
+{
+    internal class Account
+    {
+        public int NumAccount { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+
+        public Account() { }
+
+        public Account(int numAccount, string holder, double balance)
+        {
+            NumAccount = numAccount;
+            Holder = holder;
+            Balance = balance;
+        }
+
+
+        public void Withdraw (double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit (double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ CursoUdemy/Entities/SavingsAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace CursoUdemy.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InteresetRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount (int number, string holder, double balance, double interestRate) : base (number, holder, balance)
+        {
+            InteresetRate = interestRate;
+        }
+
+
+        public void UpdateBalance ()
+        {
+            Balance += Balance * InteresetRate;
+        }
+
+    }
+}
