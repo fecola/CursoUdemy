97f2e4f23fe74e252618013346317bbf4adfd08f
Felipe Colacioppo <felipe.colacioppo@outlook.com>
2023-01-17 19:56:49 -0300

    Solucao sem a interface

---
+++ CursoUdemy/Entities/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿namespace CursoUdemy.Entities
+{
+    internal class Vehicle
+    {
+
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
--- CursoUdemy/Program.cs
+++ CursoUdemy/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace CursoUdemy
+﻿using CursoUdemy.Entities;
+using CursoUdemy.Services;
+using System.Globalization;
+
+namespace CursoUdemy
 {
     internal class Program
     {
@@ -6,9 +10,41 @@
         public static void Main(String[] args)
         {
 
+            Console.WriteLine("Enter rental data:");
+            
+            Console.Write("Car model: ");
+            string modelCar = Console.ReadLine();
+
+
+            Console.Write("Pickup (dd/mm/yyyy hh:mm): ");
+            DateTime horaEntrada = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/mm/yyyy hh:mm): ");
+            DateTime horaSaida = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental rental = new CarRental(horaEntrada, horaSaida, new Vehicle(modelCar));
+
+
+
+            Console.Write("Enter price per hour: ");
+            double priceHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per day: ");
+            double priceDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            RentalService rentalService = new RentalService(priceHour, priceDay);
+
+            rentalService.ProcessInvoice(rental);
+
+
+
+            Console.WriteLine("\nINVOICE: ");
+            Console.WriteLine(rental.Invoice);
 
 
         }
 
     }
 }
+ 
